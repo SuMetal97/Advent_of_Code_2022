5f2f0081b135dc4d800aa81b9a7b7ea14071440d
SuMetal97 <wrigley1@mail.usf.edu>
2022-12-03 01:34:24 -0500

    making code way cleaner for Day 3

--- Day_3/Rutsack.py
+++ Day_3/Rutsack.py
@@ -8,31 +8,24 @@ for x, y in zip(range(27,54), string.ascii_uppercase):
 total_part1 = 0
 total_part2 = 0
 elf = []
-tracker = 0
+
 with open('input.txt', 'r') as file:
     for line in file:
         stripline = line.strip('\n')
         sack1 = stripline[0:len(stripline)//2]
         sack2 = stripline[(len(stripline)//2):len(stripline)]
+        sack1 = set(sack1)
+        sack2 = set(sack2)
         elf.append(stripline)
-        for item in sack1:
-            if item in sack2:
-                total_part1 += values[item]
-                break
-        check = False
+        matching_char = list(sack1 & sack2)
+        total_part1 += values[matching_char[0]]
         if len(elf) == 3:
-            for item1 in elf[0]:
-                if check:
-                    break
-                for item2 in elf[1]:
-                    if check:
-                        break
-                    for item3 in elf[2]:
-                        if item1 == item2 == item3:
-                            total_part2 += values[item1]
-                            check = True
-                            break
-            tracker = 0
+            elf1 = set(elf[0])
+            elf2 = set(elf[1])
+            elf3 = set(elf[2])
+            matching_char = list(elf1 & elf2 & elf3)
+
+            total_part2 += values[matching_char[0]]
             elf.clear()
 
 print("total part 1: " + str(total_part1))
